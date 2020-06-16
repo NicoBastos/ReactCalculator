93c60f52062dbb6ce85c9f4288639b34daf303fb
nickbastos <nickbastos4@gmail.com>
2020-06-16 14:48:14 -0500

    Added clear button

--- src/App.js
+++ src/App.js
@@ -1,65 +1,71 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Button from './components/Button/Button';
-import Display from './components/Display/Display';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Button from "./components/Button/Button";
+import Display from "./components/Display/Display";
 
 function App() {
   const initialState = {
     totalValue: "",
     currentNumber: "",
     nextNumber: "",
-    operation: ""
-  }
-  const [numbers, setNumbers] = useState({...initialState})
+    operation: "",
+  };
+  const [numbers, setNumbers] = useState({ ...initialState });
 
   const [displayValue, setDisplayValue] = useState(" ");
-  
+
   const numberClicked = (value) => {
-    const newState = {...numbers};
+    const newState = { ...numbers };
     const operating = newState.operation !== "";
-    if(operating){
-      newState.nextNumber += value;
-      setNumbers(newState);
-      setDisplayValue(newState.nextNumber);
-    }else{
-      console.log("else");
-      newState.currentNumber += value;
-      setNumbers(newState);
-      setDisplayValue(newState.currentNumber);
-    }
-  }
+    if (value === "." && value)
+      if (operating) {
+        newState.nextNumber += value;
+        setNumbers(newState);
+        setDisplayValue(newState.nextNumber);
+      } else {
+        console.log("else");
+        newState.currentNumber += value;
+        setNumbers(newState);
+        setDisplayValue(newState.currentNumber);
+      }
+  };
 
   const operationClicked = (value) => {
-    const newState = {...numbers};
+    const newState = { ...numbers };
     newState.operation = value;
-    setDisplayValue("")
+    setDisplayValue("");
     setNumbers(newState);
-  }
+  };
 
   const calculate = () => {
-    let newState = {...numbers};
+    let newState = { ...numbers };
     let total;
-    if(numbers.operation === "+"){
+    if (numbers.operation === "+") {
       total = Number(numbers.currentNumber) + Number(numbers.nextNumber);
     }
-    if(numbers.operation === "-"){
+    if (numbers.operation === "-") {
       total = Number(numbers.currentNumber) - Number(numbers.nextNumber);
     }
-    if(numbers.operation === "/"){
+    if (numbers.operation === "/") {
       total = Number(numbers.currentNumber) / Number(numbers.nextNumber);
     }
-    if(numbers.operation === "*"){
+    if (numbers.operation === "*") {
       total = Number(numbers.currentNumber) * Number(numbers.nextNumber);
     }
-    newState = {...initialState};
+    newState = { ...initialState };
     newState.currentNumber = total;
     setNumbers(newState);
     setDisplayValue(total);
-  }
+  };
+
+  const clear = () => {
+    setDisplayValue("");
+    setNumbers(initialState);
+  };
   return (
     <div className="App">
       <div className="calc-wrapper">
-        <Display value={displayValue}/>
+        <Display value={displayValue} />
         <div className="row">
           <Button onPress={numberClicked}>7</Button>
           <Button onPress={numberClicked}>8</Button>
@@ -70,7 +76,7 @@ function App() {
           <Button onPress={numberClicked}>4</Button>
           <Button onPress={numberClicked}>5</Button>
           <Button onPress={numberClicked}>6</Button>
-          <Button onPress={operationClicked} >*</Button>
+          <Button onPress={operationClicked}>*</Button>
         </div>
         <div className="row">
           <Button onPress={numberClicked}>1</Button>
@@ -83,6 +89,9 @@ function App() {
           <Button onPress={numberClicked}>0</Button>
           <Button onPress={calculate}>=</Button>
           <Button onPress={operationClicked}>-</Button>
+        </div>
+        <div className="row">
+          <Button onPress={clear}>CLEAR</Button>
         </div>
       </div>
     </div>
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import classes from './Button.module.css';
+import React from "react";
+import classes from "./Button.module.css";
 const Button = (props) => {
-    const isOperator = val => {
-        return !isNaN(val) || val === '.' || val === '=';
-    }
+  const isOperator = (val) => {
+    return !isNaN(val) || val === "." || val === "=" || val === "CLEAR";
+  };
 
-    const clicked = () => {
-        props.onPress(props.children);
-    }
-    const cssClass = isOperator(props.children) ? classes.button: classes.operator;
-    return(
-        <div className={cssClass} onClick={clicked}>{props.children}</div>
-    );
+  const clicked = () => {
+    props.onPress(props.children);
+  };
+  const cssClass = isOperator(props.children)
+    ? classes.button
+    : classes.operator;
+  return (
+    <div className={cssClass} onClick={clicked}>
+      {props.children}
+    </div>
+  );
 };
 
 export default Button;
